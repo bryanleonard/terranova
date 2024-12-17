749e47a39038f9c2bf9c1bd1c00e92f7b0d66e56
Bryan Leonard <bryan@forcequit.io>
2024-12-16 21:25:45 -0500

    Add the Bootstraps.

--- src/components/banner.js
+++ src/components/banner.js
@@ -2,12 +2,12 @@ import Image from 'next/image'
 
 const Banner = () => {
     return (
-      <header className="row mb-4">
-        <div className="col-5">
-            <img src="/logo.png" alt="Terranova logo"/>
+      <header className="row mb-4 mt-3">
+        <div className="col-3">
+            <img src="/logo.png" alt="Terranova logo" class="img-logo" />
         </div>
-        <div className="col-7 mt-5">
-            Providing houses all over the world.
+        <div className="col-9 d-flex align-items-center">
+            <h4 className="align-middle">Providing houses all over the world.</h4>
         </div>
       </header>
     );
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,10 +3,15 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" />
+        <link href="css/styles.css" rel="stylesheet" />
+      </Head>
       <body>
-        <Main />
-        <NextScript />
+        <div className="container">
+          <Main />
+          <NextScript />
+        </div>
       </body>
     </Html>
   );
