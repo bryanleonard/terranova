67f5156c894be2cfc6472fab96fd2cb5e99d7d59
Bryan Leonard <bryan@forcequit.io>
2024-12-18 21:52:25 -0500

    Add house list starter component

---
+++ src/components/houseList.js
@@ -0,0 +1,56 @@
+import styles from './houseList.module.scss';
+
+const houses = [
+    {
+        id: 1,
+        address: "12 Valley of Kings, Geneva",
+        country: "Switzerland",
+        price: 900000
+    },
+    {
+        id: 2,
+        address: "89 Road of Forks, Bern",
+        country: "Switzerland",
+        price: 500000
+    },
+    {
+        id: 3,
+        address: "123 Elm St, Ohio",
+        country: "United States of America",
+        price: 250000
+    }
+];
+
+
+const HouseList = () => {
+    return (
+        <>
+            <div className="row mb-2">
+                <h5 className="themeFontColor text-center">
+                    Currently on the market
+                </h5>
+            </div>
+
+            <table className={`${styles.houseList} table table-hover`}>
+                <thead>
+                    <tr>
+                        <td>Address</td>
+                        <td>Country</td>
+                        <td>Asking Price</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    {houses.map(h => (
+                        <tr key={h.id}>
+                            <td>{h.address}</td>
+                            <td>{h.country}</td>
+                            <td>{h.price}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </>
+    )
+};
+
+export default HouseList;
