269442e4746eed877e478790c5f5d9699d661278
Bryan Leonard <bryan@forcequit.io>
2025-02-06 17:57:29 -0500

    Add updates for static export

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,12 @@
 
 const nextConfig = {
   reactStrictMode: false,
+  output: "export", // Ensures the app is fully static
+  images: {
+    unoptimized: true, // Required for exporting images statically
+  },
+  basePath: "/terranove", // Replace with your actual repository name
+  assetPrefix: "/terranova/", // Required for assets to load correctly
   // sassOptions: {
   //   implementation: 'sass-embedded'
   // },
