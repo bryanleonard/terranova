1f7ee5963577f045cd948e21606561f48187f1e8
Bryan Leonard <bryan@forcequit.io>
2025-02-06 20:28:14 -0500

    Adding gh-page changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,15 @@
 /** @type {import('next').NextConfig} */
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
-  reactStrictMode: false,
-  // sassOptions: {
-  //   implementation: 'sass-embedded'
-  // },
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: isProd ? '/terranova/' : '',
+  basePath: isProd ? '/terranova' : '',
+  output: 'export'
 };
 
 export default nextConfig;
